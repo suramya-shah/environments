a9a805c94496671e357da9fa322dbc2b5d6ecce1
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2021-06-12 18:58:47 +0530

    Update release-script.sh

--- .github/workflows/release-script.sh
+++ .github/workflows/release-script.sh
@@ -1,1 +1,14 @@
-    pwd=`pwd`;echo ${{ matrix.run }}>>$pwd/folder; cat $pwd/folder | cut -d '/' -f1>>$pwd/path ;cat $pwd/folder | cut -d '-' -f2>>$pwd/ver ; s=$(cat $pwd/folder); echo "${s##*/}">>$pwd/dockerfile; echo "ver="; cat $pwd/ver;  echo "dockerfile="; cat $pwd/dockerfile ; echo "path=";cat $pwd/path; path=$(cat $pwd/path);  if [[ $(cat $pwd/dockerfile) == 'Dockerfile' ]]; then version=$(cat $path/version);  else ver=-$(cat $pwd/ver) && cd $path && version=$(cat version$ver); fi;  echo "version=$version" >> $GITHUB_ENV ;   echo "path=$path" >> $GITHUB_ENV ;rm $pwd/ver; rm $pwd/folder; rm $pwd/path; rm $pwd/dockerfile;
+    pwd=`pwd`;echo ${{ matrix.run }}>>$pwd/folder; 
+    cat $pwd/folder | cut -d '/' -f1>>$pwd/path ;
+    cat $pwd/folder | cut -d '-' -f2>>$pwd/ver ; 
+    s=$(cat $pwd/folder); echo "${s##*/}">>$pwd/dockerfile;
+    echo "ver="; cat $pwd/ver;  echo "dockerfile="; 
+    cat $pwd/dockerfile ; echo "path=";
+    cat $pwd/path; path=$(cat $pwd/path); 
+    if [[ $(cat $pwd/dockerfile) == 'Dockerfile' ]]; then version=$(cat $path/version); else ver=-$(cat $pwd/ver) && cd $path && version=$(cat version$ver); fi; 
+    echo $version | cut -d '/' -f1>>$pwd/dockerRepo;
+    dockerRepo=$(cat $pwd/dockerRepo);
+    echo "version=$version" >> $GITHUB_ENV ;   
+    echo "path=$path" >> $GITHUB_ENV ;
+     echo "dockerRepo=$dockerRepo" >> $GITHUB_ENV ;
+    rm $pwd/ver; rm $pwd/folder; rm $pwd/path; rm $pwd/dockerfile;
