5cdf2fe588167627b6797f544de9b0ee0c9bd2bc
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2021-06-12 19:12:24 +0530

    Update release-script.sh

--- .github/workflows/release-script.sh
+++ .github/workflows/release-script.sh
@@ -8,7 +8,7 @@
     if [[ $(cat $pwd/dockerfile) == 'Dockerfile' ]]; then version=$(cat $path/version); else ver=-$(cat $pwd/ver) && cd $path && version=$(cat version$ver); fi; 
     echo "version=$version" >> $GITHUB_ENV ;   
     echo "path=$path" >> $GITHUB_ENV ;
-    echo $version | cut -d ':' -f1>>$pwd/dockerRepo;
+    cat $path/version | cut -d ':' -f1>>$pwd/dockerRepo;
     dockerRepo=$(cat $pwd/dockerRepo);    
     echo "dockerRepo=$dockerRepo" >> $GITHUB_ENV ;
     rm $pwd/ver; rm $pwd/folder; rm $pwd/path; rm $pwd/dockerfile;
